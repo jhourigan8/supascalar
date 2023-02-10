d7fa32afe5a51517b0a7cfeb8f7d062849f760e5
mzhou02 <90995895+mzhou02@users.noreply.github.com>
2023-02-09 23:21:48 -0500

    Update lc4_alu.v

--- lab2b-alu/lc4_alu.v
+++ lab2b-alu/lc4_alu.v
@@ -1,161 +1,56 @@
-/* Mike Zhou (mikezhou) and Jack Hourigan (hojack) */
+/* Jack Hourigan (hojack) */
 
 `timescale 1ns / 1ps
 `default_nettype none
 
-module lc4_alu(input  wire [15:0] i_insn,
-               input wire [15:0]  i_pc,
-               input wire [15:0]  i_r1data,
-               input wire [15:0]  i_r2data,
-               output wire [15:0] o_result);
-      
-      // top level mux
-      // even though some of these cases are trivial (e.g. all zeroes)
-      // for uniformity just going to mux them anyway
-      assign o_result = 
-            (i_insn[15:12] == 4'b0000) ? branch_out:
-            (i_insn[15:12] == 4'b0001) ? arith_out:
-            (i_insn[15:12] == 4'b0010) ? cmp_out:
-            (i_insn[15:12] == 4'b0100) ? jsr_out:
-            (i_insn[15:12] == 4'b0101) ? logic_out:
-            (i_insn[15:12] == 4'b0110) ? load_out:
-            (i_insn[15:12] == 4'b0111) ? store_out:
-            (i_insn[15:12] == 4'b1000) ? jump_out:
-            (i_insn[15:12] == 4'b1001) ? const_out:
-            (i_insn[15:12] == 4'b1010) ? shift_out:
-            (i_insn[15:12] == 4'b1100) ? jump_out:
-            (i_insn[15:12] == 4'b1101) ? hiconst_out:
-            // (i_insn[15:12] == 4’b1111) ?
-            trap_out;
+module lc4_divider(input  wire [15:0] i_dividend,
+                   input  wire [15:0] i_divisor,
+                   output wire [15:0] o_remainder,
+                   output wire [15:0] o_quotient);
 
-      // divider
-      wire [15:0] divider_o_remainder;
-      wire [15:0] divider_o_quotient;
+      wire[15:0] dividends [16:0];
+      wire[15:0] quotients [16:0];
+      wire[15:0] remainders [16:0];
 
-      // only used for DIV and MOD
-      // both want Rt and Rs inputs so no need to mux
-      lc4_divider divider(
-            i_r1data,
-            i_r2data,
-            divider_o_remainder,
-            divider_o_quotient
-      );
+      assign dividends[0] = i_dividend;
+      assign quotients[0] = 16'b0;
+      assign remainders[0] = 16'b0;
 
-      // adder
-      wire [15:0] adder_a;
-      wire [15:0] adder_b;
-      wire adder_cin;
-      wire [15:0] adder_sum;
-      wire [32:0] adder_tmp;
-  
-      assign adder_tmp = 
-            (i_insn[15:12] == 4'b0000) ? {i_pc, i_insn[8] ? {7'h7F, i_insn[8:0]} : {7'b0, i_insn[8:0]}, 1'b1} :
-            (i_insn[15:12] == 4'b0001) ? 
-                  ((i_insn[5:3] == 3'b000) ? {i_r1data, i_r2data, 1'b0} :
-                  (i_insn[5:3] == 3'b010) ? {i_r1data, ~i_r2data, 1'b1} :
-                  {i_r1data, i_insn[4] ? {11'h7FF, i_insn[4:0]} : {11'b0, i_insn[4:0]}, 1'b0}) :
-            (i_insn[15:12] == 4'b0110) ? {i_r1data, i_insn[10] ? {5'h1F, i_insn[10:0]} : {5'h1F, i_insn[10:0]}, 1'b0} :
-            (i_insn[15:12] == 4'b0111) ? {i_r1data,  i_insn[10] ? {5'h1F, i_insn[10:0]} : {5'h1F, i_insn[10:0]}, 1'b0} :
-            {i_pc, {5'b0, i_insn[10:0]}, 1'b1};
+      genvar i;
+      for (i = 0; i < 16; i = i+1) begin
+            lc4_divider_one_iter iter(
+                  .i_dividend(dividends[i]),
+                  .i_divisor(i_divisor),
+                  .i_remainder(remainders[i]),
+                  .i_quotient(quotients[i]),
+                  .o_dividend(dividends[i+1]),
+                  .o_remainder(remainders[i+1]),
+                  .o_quotient(quotients[i+1])
+            );
+      end
 
-      assign adder_a = adder_tmp[32:17];
-      assign adder_a = adder_tmp[16:1];
-      assign adder_cin = adder_tmp[0];
+      assign o_remainder = i_divisor ? remainders[16] : 0;
+      assign o_quotient = i_divisor ? quotients[16] : 0;
 
-      cla16 adder(
-            adder_a,
-            adder_b,
-            adder_cin,
-            adder_sum
-      );
 
-      // 0000 -- branch
-      wire [15:0] branch_out;
-      assign branch_out = adder_sum;
+endmodule // lc4_divider
 
-      // 0001 -- arith
-      wire [15:0] arith_out;
-      assign arith_out = 
-            (i_insn[5:3] == 3'b000) ? adder_sum :
-            (i_insn[5:3] == 3'b001) ? i_r1data * i_r2data:
-            (i_insn[5:3] == 3'b010) ? adder_sum :
-            (i_insn[5:3] == 3'b011) ? divider_o_quotient :
-            // default
-            adder_sum;
+module lc4_divider_one_iter(input  wire [15:0] i_dividend,
+                            input  wire [15:0] i_divisor,
+                            input  wire [15:0] i_remainder,
+                            input  wire [15:0] i_quotient,
+                            output wire [15:0] o_dividend,
+                            output wire [15:0] o_remainder,
+                            output wire [15:0] o_quotient);
 
-      // 0010 -- cmp
-      wire [15:0] cmp_out;
-      assign cmp_out = 
-            (i_insn[8:7] == 2'b00) ? 
-                  (($signed(i_r1data) > $signed(i_r2data)) ? 
-                  16'b1 : (i_r1data == i_r2data) ? 
-                  16'b0 : 16'hFFFF) :
-            (i_insn[8:7] == 2'b01) ? 
-                  ((i_r1data > i_r2data) ? 
-                  16'b1 : (i_r1data == i_r2data) ? 
-                  16'b0 : 16'hFFFF) :
-            (i_insn[8:7] == 2'b10) ? 
-                  (($signed(i_r1data) > $signed({9'b0, i_insn[6:0]})) ? 
-                  16'b1 : (i_r1data == {9'b0, i_insn[6:0]}) ? 
-                  16'b0 : 16'hFFFF) :
-            // (i_insn[8:7] == 2'b11)
-                  ((i_r1data > {9'b0, i_insn[6:0]}) ? 
-                  16'b1 : (i_r1data == {9'b0, i_insn[6:0]}) ? 
-                  16'b0 : 16'hFFFF);
+      wire [15:0] tmp_remainder;
+      wire sub;
 
-      // 0100 -- jsr
-      wire [15:0] jsr_out;
-      assign jsr_out = 
-            (i_insn[10] == 1'b0) ? i_r1data :
-            // i_insn[10] == 1'b1
-            (i_pc & 16'h8000) | ({5'b0, i_insn[10:0]} << 4); 
+      assign tmp_remainder = {i_remainder[14:0], i_dividend[15]};
+      assign sub = tmp_remainder >= i_divisor;
+      assign o_quotient = {i_quotient[14:0], sub};
+      assign o_remainder = sub ? tmp_remainder - i_divisor : tmp_remainder;
+      assign o_dividend = i_dividend <<< 1;
 
-      // 0101 -- logic
-      wire [15:0] logic_out;
-      assign logic_out = 
-            (i_insn[5:3] == 3'b000) ? i_r1data & i_r2data :
-            (i_insn[5:3] == 3'b001) ? ~i_r1data :
-            (i_insn[5:3] == 3'b010) ? i_r1data | i_r2data :
-            (i_insn[5:3] == 3'b011) ? i_r1data ^ i_r2data :
-            // default
-            i_r1data ^ (i_insn[4] ? {11'h7FF, i_insn[4:0]} : {11'b0, i_insn[4:0]}); 
-
-      // 0110 -- load
-      wire [15:0] load_out;
-      assign load_out = adder_sum;
-
-      // 0111 -- store
-      wire [15:0] store_out;
-      assign load_out = adder_sum;
-
-      // 1000 -- rti
-      wire [15:0] rti_out;
-      assign rti_out = i_r1data;
-
-      // 1001 -- const
-      wire [15:0] const_out;
-      assign const_out = i_insn[8] ? {7'h7F, i_insn[8:0]} : {7'b0, i_insn[8:0]};
-
-      // 1010 -- shift
-      wire [15:0] shift_out;
-      assign shift_out = 
-            (i_insn[5:4] == 2'b00) ? i_r1data << i_insn[3:0] :
-            (i_insn[5:4] == 2'b01) ? i_r1data >>> i_insn[3:0] :
-            (i_insn[5:4] == 2'b10) ? i_r1data >> i_insn[3:0] :
-            divider_o_remainder; // i_insn[5:4] == 2'b11
-
-      // 1100 -- jump
-      wire [15:0] jump_out;
-      assign jump_out = 
-            (i_insn[10] == 1'b0) ? i_r1data : 
-            adder_sum; // i_insn[10] == 1'b1
-
-      // 1101 -- hiconst
-      wire [15:0] hiconst_out;
-      assign hiconst_out = (i_r1data & 16'h00FF) | ({8'b0, i_insn[7:0]} << 8);
-
-      // 1111 -- trap
-      wire [15:0] trap_out;
-      assign trap_out = 16'h8000 | {8'b0, i_insn[7:0]};
 
 endmodule
