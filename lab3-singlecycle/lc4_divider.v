7ddb35860d7603ca8312bd21e30a6afea3812f2b
Jack E Hourigan <hojack@big23.seas.upenn.edu>
2023-02-20 22:36:09 -0500

    lab3 comically easy

--- lab3-singlecycle/lc4_divider.v
+++ lab3-singlecycle/lc4_divider.v
@@ -1,4 +1,4 @@
-/* TODO: name and PennKeys of all group members here */
+/* Jack Hourigan (hojack) */
 
 `timescale 1ns / 1ps
 `default_nettype none
@@ -8,7 +8,30 @@ module lc4_divider(input  wire [15:0] i_dividend,
                    output wire [15:0] o_remainder,
                    output wire [15:0] o_quotient);
 
-      /*** YOUR CODE HERE ***/
+      wire[15:0] dividends [16:0];
+      wire[15:0] quotients [16:0];
+      wire[15:0] remainders [16:0];
+
+      assign dividends[0] = i_dividend;
+      assign quotients[0] = 16'b0;
+      assign remainders[0] = 16'b0;
+
+      genvar i;
+      for (i = 0; i < 16; i = i+1) begin
+            lc4_divider_one_iter iter(
+                  .i_dividend(dividends[i]),
+                  .i_divisor(i_divisor),
+                  .i_remainder(remainders[i]),
+                  .i_quotient(quotients[i]),
+                  .o_dividend(dividends[i+1]),
+                  .o_remainder(remainders[i+1]),
+                  .o_quotient(quotients[i+1])
+            );
+      end
+
+      assign o_remainder = i_divisor ? remainders[16] : 0;
+      assign o_quotient = i_divisor ? quotients[16] : 0;
+
 
 endmodule // lc4_divider
 
@@ -20,6 +43,14 @@ module lc4_divider_one_iter(input  wire [15:0] i_dividend,
                             output wire [15:0] o_remainder,
                             output wire [15:0] o_quotient);
 
-      /*** YOUR CODE HERE ***/
+      wire [15:0] tmp_remainder;
+      wire sub;
+
+      assign tmp_remainder = {i_remainder[14:0], i_dividend[15]};
+      assign sub = tmp_remainder >= i_divisor;
+      assign o_quotient = {i_quotient[14:0], sub};
+      assign o_remainder = sub ? tmp_remainder - i_divisor : tmp_remainder;
+      assign o_dividend = i_dividend <<< 1;
+
 
 endmodule
--- lab3-singlecycle/lc4_single.v
+++ lab3-singlecycle/lc4_single.v
@@ -63,11 +63,98 @@ module lc4_processor
 
    /* END DO NOT MODIFY THIS CODE */
 
+   wire [ 2:0] r1sel;              // rs
+   wire        r1re;               // does this instruction read from rs?
+   wire [ 2:0] r2sel;              // rt
+   wire        r2re;               // does this instruction read from rt?
+   wire [ 2:0] wsel;               // rd
+   wire        regfile_we;         // does this instruction write to rd?
+   wire        nzp_we;             // does this instruction write the NZP bits?
+   wire        select_pc_plus_one; // write PC+1 to the regfile?
+   wire        is_load;            // is this a load instruction?
+   wire        is_store;           // is this a store instruction?
+   wire        is_branch;          // is this a branch instruction?
+   wire        is_control_insn;    // is this a control instruction (JSR, JSRR, RTI, JMPR, JMP, TRAP)?
 
-   /*******************************
-    * TODO: INSERT YOUR CODE HERE *
-    *******************************/
+   lc4_decoder decoder (
+      .insn(i_cur_insn),
+      .r1sel(r1sel),
+      .r1re(r1re),
+      .r2sel(r2sel),
+      .r2re(r2re),
+      .wsel(wsel),
+      .regfile_we(regfile_we),
+      .nzp_we(nzp_we),
+      .select_pc_plus_one(select_pc_plus_one),
+      .is_load(is_load),
+      .is_store(is_store),
+      .is_branch(is_branch),
+      .is_control_insn(is_control_insn)
+   );
 
+   wire [15:0] pc_plus_one;
+   cla16 pc_inc(pc, 16'b0, 1'b1, pc_plus_one);
+
+   wire [15:0] wdata;
+   assign wdata = is_load ? i_cur_dmem_data : (select_pc_plus_one ? pc_plus_one : alu_res);
+
+   wire [15:0] rs_data;
+   wire [15:0] rt_data;
+
+   lc4_regfile regfile (
+      .clk(clk),
+      .gwe(gwe),
+      .rst(rst),
+      .i_rs(r1sel),
+      .o_rs_data(rs_data),
+      .i_rt(r2sel),
+      .o_rt_data(rt_data),
+      .i_rd(wsel),
+      .i_wdata(wdata),
+      .i_rd_we(regfile_we)
+   );
+
+   wire [15:0] alu_res;
+
+   lc4_alu alu (
+      .i_insn(i_cur_insn),
+      .i_r1data(rs_data),
+      .i_r2data(rt_data),
+      .i_pc(pc),
+      .o_result(alu_res)
+   );
+
+   wire [2:0] nzp;
+   wire n_in;
+   wire z_in;
+   wire p_in;
+   assign n_in = (wdata[15] == 1'b1);
+   assign z_in = (wdata == 16'b0);
+   assign p_in = ~n_in & ~z_in;
+
+   Nbit_reg #(3, 0) nzp_reg (.in({n_in, z_in, p_in}), .out(nzp), .clk(clk), .we(nzp_we), .gwe(gwe), .rst(rst));
+
+   wire nzp_test;
+   assign nzp_test = (i_cur_insn[11] & nzp[2]) | (i_cur_insn[10] & nzp[1]) | (i_cur_insn[9] & nzp[0]);
+
+   assign next_pc = (is_control_insn | (is_branch & nzp_test)) ? alu_res : pc_plus_one;
+
+   assign o_dmem_addr = (is_load | is_store) ? alu_res : 16'b0;
+   assign o_dmem_we = is_store;
+   assign o_dmem_towrite = is_store ? rt_data : (is_load ? wdata : 16'b0);
+   assign o_cur_pc = pc;
+
+   assign test_stall = 2'b0;
+   assign test_cur_pc = pc;
+   assign test_cur_insn = i_cur_insn;
+   assign test_regfile_we = regfile_we;
+   assign test_regfile_wsel = wsel;
+   assign test_regfile_data = wdata;
+   assign test_nzp_we = nzp_we;
+   assign test_nzp_new_bits = {n_in, z_in, p_in};
+   assign test_dmem_we = o_dmem_we;
+   assign test_dmem_addr = o_dmem_addr;
+   assign test_dmem_data = o_dmem_towrite;
 
 
    /* Add $display(...) calls in the always block below to
